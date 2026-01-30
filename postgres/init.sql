28707ad61f92682ac59bf6e40b14437ba763387d
beydakkk <beydakk@gmail.com>
2026-01-30 20:10:36 +0300

    Create init.sql file

---
+++ postgres/init.sql
@@ -0,0 +1,23 @@
+-- Copy&Paste this file content to your PostgreSQL initialization script
+CREATE TABLE IF NOT EXISTS publisher (
+    publisher_id BIGSERIAL PRIMARY KEY,
+    publisher_name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS book (
+    book_id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    price NUMERIC(12,2),
+    isbn13 VARCHAR(20) UNIQUE,
+    published_year INT,
+    publisher_id BIGINT NOT NULL REFERENCES publisher(publisher_id)
+);
+
+CREATE TABLE IF NOT EXISTS author (
+    author_id BIGSERIAL PRIMARY KEY,
+    author_name_surname VARCHAR(255) NOT NULL,
+    book_id BIGINT NOT NULL UNIQUE REFERENCES book(book_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_book_title ON book(title);
+CREATE INDEX IF NOT EXISTS idx_book_published_year ON book(published_year);
