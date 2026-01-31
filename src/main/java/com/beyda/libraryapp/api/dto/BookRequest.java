fbcde2c6854f29a324a3dbb476fef2a376b3e555
beydakkk <beydakk@gmail.com>
2026-01-31 21:12:41 +0300

    Change type of 'price'

--- src/main/java/com/beyda/libraryapp/api/dto/BookRequest.java
+++ src/main/java/com/beyda/libraryapp/api/dto/BookRequest.java
@@ -1,10 +1,11 @@
 package com.beyda.libraryapp.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import java.math.BigDecimal;
 
 public record BookRequest(
         @NotBlank String title,
-        Double price,
+        BigDecimal price,
         String ISBN13,
         Integer publishedYear,
         @NotBlank String publisherName,
