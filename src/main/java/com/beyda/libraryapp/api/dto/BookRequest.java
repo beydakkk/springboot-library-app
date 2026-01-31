f5f05e546cac0977caa354baac2bfd32137e66e2
beydakkk <beydakk@gmail.com>
2026-01-31 14:23:09 +0300

    Change file name

--- src/main/java/com/beyda/libraryapp/api/dto/BookRequest.java
+++ src/main/java/com/beyda/libraryapp/api/dto/BookRequest.java
@@ -2,7 +2,7 @@ package com.beyda.libraryapp.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record BookCreateRequest(
+public record BookRequest(
         @NotBlank String title,
         Double price,
         String ISBN13,
